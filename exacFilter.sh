995d3775375aa9fb65180d304d7c8826813894a0
Nicholas D. Socci <soccin@mskcc.org>
2016-04-10 21:17:47 -0400

    rewrote main script

--- exacFilter.sh
+++ exacFilter.sh
@@ -4,29 +4,20 @@ SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 function usage {
 	echo
-	echo "exacFilter.sh [-d] TITLEFILE INPUT.MAF OUTPUT.MAF"
+	echo "exacFilter.sh [-d] GENOME INPUT.MAF OUTPUT.MAF"
 	echo "    -d turn on debug mode"
 	echo
+	echo "Filter for ExAC common variants"
 	exit
 }
 
-#
-# Need this version to get the FILTER column
-#
-
-VCF2MAF=/opt/common/CentOS_6-dev/vcf2maf/v1.6.5
-
-PERL=/opt/common/CentOS_6-dev/perl/perl-5.22.0/bin/perl
-VEPPATH=/opt/common/CentOS_6/vep/v83
-MSK_ISOFORMS=$VCF2MAF/data/isoform_overrides_at_mskcc
-GENOME=/ifs/depot/assemblies/H.sapiens/b37/b37.fasta
-GENOMEFAI=/ifs/depot/assemblies/H.sapiens/b37/b37.fasta.fai
-
 DEBUG="No"
+DEBUGOPT=""
 while getopts "dh" opt; do
 	case $opt in
 		d)
 			DEBUG="Yes"
+			DEBUGOPT="-d"
 			;;
 		h)
 			usage
@@ -40,67 +31,39 @@ done
 shift $((OPTIND - 1))
 if [ "$#" != "3" ]; then
 	usage
-	exit
+	exit 1
 fi
 
-TITLEFILE=$1
+GENOME=$1
 INPUT_MAF=$2
 OUTPUT_MAF=$3
+
+case $GENOME in
+	b37)
+		GENOME=/ifs/depot/assemblies/H.sapiens/b37/b37.fasta
+		GENOMEFAI=/ifs/depot/assemblies/H.sapiens/b37/b37.fasta.fai
+		;;
+	*)
+		echo "Invalid Genome" $GENOME
+		exit 1
+		;;
+esac
+
 
 #
 # Get a uniq temp directory for scratch
 #
 
-TDIR=_scratch_$(uuidgen -t)
+TDIR=_scratch_exactFilter_$(uuidgen -t)
 mkdir -p $TDIR
 
+$SDIR/annotateMAF.sh $DEBUGOPT $GENOME $INPUT_MAF $TDIR/annotate.maf
+$SDIR/filterMAFExAC.py $INPUT_MAF $TDIR/annotate.maf $OUTPUT_MAF
+
 #
-# Make a temp symlink of genome to deal with BIOPERL's
-# directory permissions problems.
-#
+# If DEBUG off then cleanup
+# 
 
-ln -s $GENOME $TDIR/$(basename $GENOME)
-ln -s ${GENOME}.fai $TDIR/$(basename $GENOME).fai
-
-echo "Running VCF2MAF ..."
-$PERL $VCF2MAF/maf2maf.pl \
-    --vep-forks 12 \
-    --tmp-dir $TDIR/SOM \
-    --vep-path $VEPPATH \
-	--vep-data $VEPPATH \
-	--ref-fasta $TDIR/$(basename $GENOME) \
-	--retain-cols Center,Verification_Status,Validation_Status,Mutation_Status,Sequencing_Phase,Sequence_Source,Validation_Method,Score,BAM_file,Sequencer,Tumor_Sample_UUID,Matched_Norm_Sample_UUID,Caller \
-    --custom-enst $MSK_ISOFORMS \
-	--input-maf $INPUT_MAF \
-	--output-maf $TDIR/vep.maf \
-	2> $TDIR/STDERR_VCF2MAF \
-
-ERROR_FLAG=$(egrep "ERROR" $TDIR/STDERR_VCF2MAF)
-if [ "$ERROR_FLAG" != "" ]; then
-	echo
-	echo "FATAL ERROR: exactFilter.sh::VCF2MAF"
-	echo
-	echo $ERROR_FLAG
-	echo
-	echo "SCRATCH DIR = "$TDIR
-	echo
-	exit 1
-fi
-
-$SDIR/filterMAFExAC.py $TITLEFILE $TDIR/vep.maf $OUTPUT_MAF
-
-ERRORNO=$?
-if [ "$ERRORNO" != "0" ]; then
-	echo
-	echo "FATAL ERROR: exactFilter.sh::filterMAFExAC.py"
-	echo
-	echo "SCRATCH DIR = "$TDIR
-	echo
-	rm -f $OUTPUT_MAF
-	exit 1
-fi
-
-echo "done"
 if [ "$DEBUG" == "No" ]; then
 	rm -rf $TDIR
 fi
